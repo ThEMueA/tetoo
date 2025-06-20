cd432e00408ae1b627ee1c3e0fbf42499bc44021
ThEMueA <100469275+ThEMueA@users.noreply.github.com>
2025-06-20 17:19:36 +0300

    ss

---
+++ ddd.bat
@@ -0,0 +1,10 @@
+mkdir folder_name
+
+# Create nested folders
+mkdir -p path/to/nested/folder
+
+# Delete empty folder
+rmdir folder_name
+
+# Delete folder recursively (DANGEROUS!)
+rm -rf folder_name
